6b387dc63591678a4e62c346fa8ccf3b50238703
Guillermo Calderon <t-guillermoc@microsoft.com>
2023-06-13 11:32:29 -0700

    Initial Commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,27 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+use cxx::{CxxString, CxxVector};
 
 #[cxx::bridge]
 mod ffi {
     extern "Rust"
     {
-        fn log_message_from_rust_log_crate() -> ();
+        fn log_message_from_rust_log_crate(level: i32, message: &CxxString, attributes: &CxxVector<CxxString>);
         fn init_rust_logger() -> ();
     }
 }
 // pass an argument from cpp to rust (string, int and others std::map)
-pub fn log_message_from_rust_log_crate() -> ()
+pub fn log_message_from_rust_log_crate(level: i32, message: &CxxString, attributes: &CxxVector<CxxString>)
 {
-    info!("Log from log crate library RUST");
+    match level
+    {
+        1 => info!("{} {:?}", message, attributes),
+        2 => debug!("{} {:?}", message, attributes),
+
+        _ => warn!("Invalid log level: {} Message: {} Attributes: {:?}", level, message, attributes),
+    }
+    // info!("Log from log crate library RUST");
 }
 
 pub fn init_rust_logger() -> ()
--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,19 @@
 
 // consider the ffi part of Rust code
 #include "cpprust/src/lib.rs.h"
+#include <vector>
+#include <string>
 
 #include <iostream>
 
 int main()
 {
+    int level = 2;
+    std::string message = "This is a test message";
+    std::vector<std::string> attributes = {"Attribute1", "Attribute2", "Attribute3"}; 
     init_rust_logger();
     std::cout << "starting from C++\n";
-    log_message_from_rust_log_crate();
+    log_message_from_rust_log_crate(level, message, attributes);
     std::cout << "finishing with C++\n";
     return 0;
 }
