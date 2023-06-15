163f617c15401d202f7f9d6f2e93ceb88643a36c
Guillermo Calderon <t-guillermoc@microsoft.com>
2023-06-14 23:31:25 -0700

    Add Google Benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,22 @@
 */
 
 #include "cpprust/src/lib.rs.h"
+#include <benchmark/benchmark.h>
 #include <vector>
 #include <string>
 #include <iostream>
 
-int main()
-{
-    MyClass my_object(42);
-    print_value_from_rust(my_object);
+static bool logger_initialized = false;
 
+void init_logger_once() {
+    if (!logger_initialized) {
+        init_rust_logger();
+        logger_initialized = true;
+    }
+}
+
+static void BM_log_vector_from_rust_log_crate(benchmark::State& state)
+{
     std::vector<Person> people;
 
     Person p1;
@@ -34,16 +41,49 @@ int main()
     Person p3;
     p3.name = "Aaron";
     p3.age = 25;
-    people.push_back(p3);    
+    people.push_back(p3);
+
+    init_logger_once();
+
+    for (auto _ : state) {
+        log_vector_from_rust_log_crate(people);
+    }
+}
+
+BENCHMARK(BM_log_vector_from_rust_log_crate);
+
+int main(int argc, char** argv)
+{
+    ::benchmark::Initialize(&argc, argv);
+    ::benchmark::RunSpecifiedBenchmarks();
+    // MyClass my_object(42);
+    // print_value_from_rust(my_object);
+
+    // std::vector<Person> people;
+
+    // Person p1;
+    // p1.name = "John";
+    // p1.age = 30;
+    // people.push_back(p1);
+
+    // Person p2;
+    // p2.name = "Bob";
+    // p2.age = 40;
+    // people.push_back(p2);
+
+    // Person p3;
+    // p3.name = "Aaron";
+    // p3.age = 25;
+    // people.push_back(p3);    
 
     
-    int level = 5;
-    std::string message = "This is a test message";
-    std::vector<std::string> attributes = {"Attribute1", "Attribute2", "Attribute3"};
+    // int level = 5;
+    // std::string message = "This is a test message";
+    // std::vector<std::string> attributes = {"Attribute1", "Attribute2", "Attribute3"};
 
-    init_rust_logger();
-    log_vector_from_rust_log_crate(people);
-    log_struct_from_rust_log_crate(p1);
-    log_message_from_rust_log_crate(level, message, attributes);
+    // init_rust_logger();
+    // log_vector_from_rust_log_crate(people);
+    // log_struct_from_rust_log_crate(p1);
+    // log_message_from_rust_log_crate(level, message, attributes);
     return 0;
 }
