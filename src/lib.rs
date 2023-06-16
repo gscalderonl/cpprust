aa1fa6afda191cf3871372d74d4164fee56e012f
Guillermo Calderon <t-guillermoc@microsoft.com>
2023-06-16 10:40:20 -0700

    Minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,6 @@
 extern crate log;
 extern crate env_logger;
 use cxx::{CxxString, CxxVector};
-use std::fs::OpenOptions;
-use std::io::Write;
-use std::sync::Mutex;
 
 #[cxx::bridge]
 mod ffi
@@ -45,44 +42,45 @@ pub fn log_string_from_cpp_to_rust_log_crate(message: &CxxString)
 
 pub fn log_int_from_cpp_to_rust_log_crate(level: i32)
 {
-    debug!("{}", level);
+    info!("{}", level);
 }
 
 pub fn log_vector_from_cpp_to_rust_log_crate(attributes: &CxxVector<CxxString>)
 {
-    warn!("{:?}", attributes);
+    info!("{:?}", attributes);
 }
 
 pub fn log_struct_from_cpp_to_rust_log_crate(person: &ffi::Person)
 {
-    trace!("Received persons: {} who is {} years old", person.name, person.age);
+    info!("Received persons: {} who is {} years old", person.name, person.age);
 }
 
 pub fn log_class_from_cpp_to_rust_log_crate(animal: &ffi::Animal)
 {
     let value = animal.get_age();
-    error!("{}", value);
+    info!("{}", value);
 }
 
 pub fn init_rust_logger() -> ()
 {
-    let file = OpenOptions::new()
-        .write(true)
-        .append(false)
-        .create(true)
-        .open("/dev/null")
-        .unwrap();
+    let logger = Box::new(NoopLogger);
 
-    let file = Mutex::new(file);
-
-    let logger = env_logger::Builder::new()
-        .format(move |_buf, record| {
-            let mut file = file.lock().unwrap();
-            writeln!(file, "{}: {}", record.level(), record.args())
-        })
-        .filter(None, log::LevelFilter::Trace)
-        .build();
-    
-    log::set_boxed_logger(Box::new(logger)).unwrap();
+    log::set_boxed_logger(logger).unwrap();
     log::set_max_level(log::LevelFilter::Trace);
+}
+
+struct NoopLogger;
+
+impl log::Log for NoopLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, _record: &log::Record) {
+        // do nothing
+    }
+
+    fn flush(&self) {
+        // do nothing
+    }
 }
